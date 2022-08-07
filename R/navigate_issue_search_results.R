70e615ee858b36f29f5bd5ae50243521836ede7a
Miles McBain <miles.mcbain@gmail.com>
2022-08-07 23:29:09 +1000

    navigation stubs

---
+++ R/navigate_issue_search_results.R
@@ -0,0 +1,7 @@
+issue_search_results_forward <- function() {}
+
+issue_search_results_backward <- function() {}
+
+issue_search_results_expand <- function() {}
+
+issue_search_results_collapse <- function() {}
