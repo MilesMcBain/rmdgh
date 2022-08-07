07f0c50c34a6f183e6a8920ffb61df4143f4b352
Miles McBain <miles.mcbain@gmail.com>
2022-08-07 23:27:58 +1000

    new cache behaviour, return object and inject key

--- R/results_cache.R
+++ R/results_cache.R
@@ -4,11 +4,16 @@ RESULTS_CACHE <- new.env()
 
 cache_result <- function(gh_result_object) {
   key <- uuid::UUIDgenerate()
+  gh_result_object$cache_key <- key
   assign(key, gh_result_object, envir = RESULTS_CACHE)
+  gh_result_object
 }
 
 get_cached_result <- function(key) {
   get(key, envir = RESULTS_CACHE)
 }
 
+clear_results_cache <- function() {
+  rm(list = ls(RESULTS_CACHE), envir = RESULTS_CACHE)
+}
 
