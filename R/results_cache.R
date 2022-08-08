ed076606814ec5704b534f38a27117338d2e2758
Miles McBain <miles.mcbain@gmail.com>
2022-08-08 22:40:58 +1000

    add cache update function

--- R/results_cache.R
+++ R/results_cache.R
@@ -9,6 +9,11 @@ cache_result <- function(gh_result_object) {
   gh_result_object
 }
 
+update_cached_result <- function(gh_result_object, key) {
+  assign(key, gh_result_object, envir = RESULTS_CACHE)
+  gh_result_object
+}
+
 get_cached_result <- function(key) {
   get(key, envir = RESULTS_CACHE)
 }
