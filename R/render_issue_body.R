8f71222bfcdae9121b7af28c2826e42afda3f857
Miles McBain <miles.mcbain@gmail.com>
2022-08-13 21:49:55 +1000

    remove extra 'ago'

--- R/render_issue_body.R
+++ R/render_issue_body.R
@@ -1,7 +1,7 @@
 render_issue_body <- function(issue) {
 
   glue::glue("-----\n",
-             "*@{issue$author} wrote {time_diff(issue$created_at)} ago*:\n",
+             "*@{issue$author} wrote {time_diff(issue$created_at)}*:\n",
              "{issue$body}\n",
              "-----\n", 
              .trim = FALSE)
