5a1ceab7558d4fc5741299476b373605a8a78b04
Miles McBain <miles.mcbain@gmail.com>
2022-08-17 00:43:20 +1000

    fix file cleanup timing bug

--- R/documents.R
+++ R/documents.R
@@ -40,6 +40,6 @@ md_document <- function(document_tile) {
   pkg_user_dir <- get_pkg_user_dir()
   file_info_df <- fs::file_info(list.files(pkg_user_dir, full.names = TRUE))
   # Remove all files from previous days
-  old_files <- as.numeric(Sys.Date() - as.Date(file_info_df$birth_time)) > 0
+  old_files <- as.numeric(Sys.Date() - as.Date(file_info_df$birth_time, tz = Sys.timezone())) > 0
   unlink(file_info_df[old_files, ]$path)
 }
