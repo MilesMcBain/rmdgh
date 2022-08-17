9d79cdad5d6c78c062972f738c27b1642be62dfe
Miles McBain <miles.mcbain@gmail.com>
2022-08-17 21:32:34 +1000

    default repo if possible in draft_issue()
    - fixes #5

--- R/draft_issue.R
+++ R/draft_issue.R
@@ -2,7 +2,7 @@
 draft_issue <- function(filename = "issue.Rmd", tempdir = TRUE, overwrite = TRUE) {
   file_path <- if (tempdir) {
     file.path(get_pkg_user_dir(), filename)
-  }  else {
+  } else {
     file.path(getwd(), filename)
   }
 
@@ -18,6 +18,29 @@ draft_issue <- function(filename = "issue.Rmd", tempdir = TRUE, overwrite = TRUE
     edit = FALSE
   )
 
+  default_repo <- get_repo_remote()
+  if (!is.null(default_repo)) {
+    swap_repo_yaml(file_path, default_repo)
+  }
+
   rstudioapi::navigateToFile(file_path)
 
+}
+
+
+swap_repo_yaml <- function(file_path, default_repo) {
+  document_yaml <- rmarkdown::yaml_front_matter(file_path)
+  yaml_length <- length(strsplit(yaml::as.yaml(document_yaml), "\n")[[1]]) + 2 # + 2 for "---"
+  document_yaml$output$`issuecreep::github_issue`$repo <- default_repo
+  document_lines <- xfun::read_utf8(file_path)
+  new_document_lines <- c(
+   paste0( 
+     "---\n",
+      yaml::as.yaml(document_yaml),
+      "---"
+      ),
+    document_lines[-seq(yaml_length)]
+  )
+  unlink(file_path)
+  xfun::write_utf8(new_document_lines, file_path)
 }
