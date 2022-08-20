571161f6ceacb58a26d6dbee582d79d607c83338
Miles McBain <miles.mcbain@gmail.com>
2022-08-20 13:34:11 +1000

    special handling for reprex code length 1 and 0

--- R/register_reprex_knitr_engine.R
+++ R/register_reprex_knitr_engine.R
@@ -2,9 +2,21 @@
 register_reprex_knitr_engine <- function() {
   knitr::knit_engines$set(reprex = function(options) {
 
+    if (length(options$code) == 0) {
+      stop("No code supplied to {reprex} chunk")
+    }
+
+    reprex_code <- if(length(options$code) == 1) {
+      c(options$code, "\n")
+      # so that if user supplied 1 line of code it's not interpreted as file path
+    } else {
+      options$code
+    }
+
+
     reprex_output <- 
       reprex::reprex(
-        input = options$code, 
+        input = reprex_code,
         venue = "gh",
         html_preview = FALSE,
         tidyverse_quiet = TRUE,
